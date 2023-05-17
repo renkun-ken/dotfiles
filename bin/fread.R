7375163e1b103adc407cabdf715dccd2eada361f
Kun Ren <renkun@outlook.com>
2023-05-17 17:47:45 +0800

    Update fread

--- bin/fread.R
+++ bin/fread.R
@@ -1,6 +1,11 @@
-suppressPackageStartupMessages({
-  requireNamespace("data.table")
-})
+library(data.table, quietly = TRUE)
 options(width = as.integer(system2("tput", args = "cols", stdout = TRUE)))
 args <- commandArgs(trailingOnly = TRUE)
-data.table::fread(args[[1]])
+dt <- data.table::fread(args[[1]])
+
+for (arg in args[-1]) {
+  expr <- parse(text = sprintf("dt <- dt[%s]", arg))
+  eval(expr)
+}
+
+print(dt)
